4c2c7a9c7ba487a37cc58a09c8e0597661115794
James Young <jyyoung@jyymail.com>
2025-08-22 23:44:32 -0400

    feat: add cert cards

--- frontend/src/components/About/Certifications/Certifications.tsx
+++ frontend/src/components/About/Certifications/Certifications.tsx
@@ -1,26 +1,32 @@
 import "./Certifications.css";
+import awsLogo from "../../../assets/icons/certs/aws-saa-logo.png";
+import rhcsaLogo from "../../../assets/icons/certs/rhcsa-logo.png";
 
 const Certifications = () => {
   return (
     <div className="certifications">
-      <ul>
-        <li>
-          <a
-            href="https://www.credly.com/badges/8c67424d-21d9-49f8-8f44-9f1467c67c58/public_url"
-            target="_blank"
-          >
-            <span>Red Hat Certified System Administrator (RHCSA)</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://www.credly.com/badges/3db937a4-b8be-4946-b739-1a41739debaf/public_url"
-            target="_blank"
-          >
-            <span>AWS Certified Solutions Architect (AWS SAA-03)</span>
-          </a>
-        </li>
-      </ul>
+        <a
+          href="https://www.credly.com/badges/8c67424d-21d9-49f8-8f44-9f1467c67c58/public_url"
+          target="_blank"
+          className="certification-item"
+        >
+          <span>Red Hat Certified System Administrator</span>
+          <img
+            src={rhcsaLogo}
+            alt="(RHCSA)"
+          />
+        </a>
+        <a
+          href="https://www.credly.com/badges/3db937a4-b8be-4946-b739-1a41739debaf/public_url"
+          target="_blank"
+          className="certification-item"
+        >
+          <span>AWS Certified Solutions Architect</span>
+          <img
+            src={awsLogo}
+            alt="(AWS SAA-03)"
+          />
+        </a>
     </div>
   );
 };
